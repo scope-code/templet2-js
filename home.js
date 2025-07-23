035e16bd83ff1f0f676592871b08304765820054
Yuossef Ahmed <scopecode.info@gmail.com>
2025-07-23 05:04:48 +0300

    done

---
+++ home.js
@@ -0,0 +1,35 @@
+// let name = ['ahmed', 'mohamed', 'yuossef'];
+// let number = ['18', '30', '22'];
+// let container = document.createElement('div');
+// document.body.appendChild(container);
+
+// function element(name,number) {
+//     let card = document.createElement('div');
+//     let title = document.createElement('h2');
+//     let age = document.createElement('p');
+//     let img = document.createElement('img');
+//     let head = document.createTextNode(name);
+//     let ageContent = document.createTextNode(number);
+
+//     card.appendChild(title);
+//     card.appendChild(age);
+//     card.appendChild(img);
+//     container.appendChild(card);
+
+//     title.appendChild(head);
+//     age.appendChild(ageContent);
+//     img.src = 'img/image 15.png';
+
+//     card.style.width = '200px';
+//     card.style.background = '#444';
+//     card.style.color = '#fff';
+//     card.style.padding = '10px';
+//     card.style.margin = '2px';
+//     card.style.display = 'inline-block';
+//     card.style.textAlign = 'center';
+//     img.style.width = '100%';
+
+// }
+// for (let i = 0; i < 8; i++) {
+//     element(name[i],number[i]);
+// }
---
+++ js/home.js
@@ -0,0 +1,20 @@
+
+// let i=2;
+// if( i == 2){
+//     console.log(3);
+// }else{
+//     console.log("none");
+// }
+
+
+// let name = ["bmw", "marseds", "honda"];
+// let model = ["2021", "2022", "2023"];
+
+// for( let i = 0 ; i < name.length ; i++ ){
+//     console.log(name[i]);
+
+//     for( let u = 0 ; u < model.length ; u++ ){
+//         console.log(model[u]);
+//     }
+
+// }
